e1a3e042f48a564a962a64ee7843b0a12070b465
Vanderson Gomes da Silva <vandersongomes169@gmail.com>
2026-06-16 18:01:26 -0300

    Update database.js

--- database.js
+++ database.js
@@ -1,1 +1,25 @@
+import sqlite3 from "sqlite3";
 
+const db = new sqlite3.Database("./database.sqlite");
+
+db.serialize(() => {
+
+    db.run(`
+    CREATE TABLE IF NOT EXISTS posts(
+        id TEXT PRIMARY KEY,
+        author TEXT,
+        likes INTEGER DEFAULT 0
+    )
+    `);
+
+    db.run(`
+    CREATE TABLE IF NOT EXISTS post_likes(
+        post_id TEXT,
+        user_id TEXT,
+        PRIMARY KEY(post_id, user_id)
+    )
+    `);
+
+});
+
+export default db;
